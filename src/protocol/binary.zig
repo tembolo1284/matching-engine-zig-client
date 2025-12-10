79b58f023dc430c34f8e9563e59a8968e65f3fe6
Paul Lopez <nik.lopez@gmail.com>
2025-12-10 18:12:44 -0500

    Updated test.

--- src/protocol/binary.zig
+++ src/protocol/binary.zig
@@ -421,8 +421,8 @@ test "decode binary top of book" {
     data[10] = 'B'; // side
     data[19] = 0; // padding
 
-    std.mem.writeInt(u32, data[12..16], 10000, .big);
-    std.mem.writeInt(u32, data[16..20], 100, .big);
+    std.mem.writeInt(u32, data[11..15], 10000, .big);
+    std.mem.writeInt(u32, data[15..19], 100, .big);
 
     const msg = try decodeOutput(&data);
     try std.testing.expectEqual(types.OutputMsgType.top_of_book, msg.msg_type);
